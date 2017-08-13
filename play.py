12cf035374ae192cddc1da682fc48c86ed6c370a
Thomas Thomassen <thomas@thomthom.net>
2017-08-13 16:34:09 +0200

    Clean up the prototype code a bit.

--- play.py
+++ play.py
@@ -1,51 +1,57 @@
-import datetime
-from dateutil.relativedelta import relativedelta
-
-import os
-
-from pprint import pprint
-
-from tinydb import TinyDB, Query
-# from tinydb.storages import JSONStorage
-from tinydb_serialization import SerializationMiddleware
-from datetime_serializer import DateTimeSerializer
-
-project_path = os.path.dirname(__file__)
-
-serialization = SerializationMiddleware()
-serialization.register_serializer(DateTimeSerializer(), 'TinyDate')
-
-db_file = os.path.join(project_path, 'database', 'play.json')
-# if not os.path.exists(db_file):
-#     os.mknod(db_file)
-db = TinyDB(db_file, storage=serialization)
-
-from flask import Flask, render_template
-app = Flask(__name__)
-
+# Boardgame Play List
+#
 # https://www.raspberrypi.org/learning/python-web-server-with-flask/worksheet/
-
+#
 # set FLASK_APP=play.py
 # set FLASK_DEBUG=1
 # flask ruin
 
+import datetime
+import os
+
+from dateutil.relativedelta import relativedelta
+
+from tinydb import TinyDB, Query
+from tinydb_serialization import SerializationMiddleware
+from datetime_serializer import DateTimeSerializer
+
+from flask import Flask, render_template
+
+################################################################################
+# Setup:
+
+# Paths
+project_path = os.path.dirname(__file__)
+
+# Database Serialization
+serialization = SerializationMiddleware()
+serialization.register_serializer(DateTimeSerializer(), 'TinyDate')
+
+# Database Config
+db_file = os.path.join(project_path, 'database', 'play.json')
+db = TinyDB(db_file, storage=serialization)
+
+# Server
+app = Flask(__name__)
+
+################################################################################
+# Utilities:
 
 def seed_dummy():
     start_time = datetime.datetime.now() + datetime.timedelta(minutes=32)
     row = {
-            'game' : {
-                'title' : 'Exploding Kittens',
-                'url' : 'https://boardgamegeek.com/boardgame/172225/exploding-kittens'
-            },
-            # 'start_time' : start_time.isoformat(), # ISO-8601
-            'start_time' : start_time, # ISO-8601
-            'players' : {
-                'registered' : ['Thom', 'David'],
-                'min' : 2,
-                'max' : 5,
-            },
-            'winner' : None
-        }
+        'game' : {
+            'title' : 'Exploding Kittens',
+            'url' : 'https://boardgamegeek.com/boardgame/172225/exploding-kittens'
+        },
+        'start_time' : start_time,
+        'players' : {
+            'registered' : ['Thom', 'David'],
+            'min' : 2,
+            'max' : 5,
+        },
+        'winner' : None
+    }
     table = db.table('matches')
     table.insert(row)
 
@@ -53,17 +59,19 @@ def seed_dummy():
 def get_matches():
     table = db.table('matches')
     result = table.all()
+    # TODO: Remove debug seeding.
     if not result:
         seed_dummy()
         result = table.all()
     return result
 
+################################################################################
+# Template Filters:
+
 # TODO: Clean up!
 @app.template_filter('humantime')
 def human_readable(start_time):
     now = datetime.datetime.now()
-    pprint(now)
-    pprint(start_time)
     delta = relativedelta(start_time, now)
     attrs = ['years', 'months', 'days', 'hours', 'minutes', 'seconds']
     hr = lambda delta: [
@@ -75,29 +83,11 @@ def human_readable(start_time):
     day_time = '{:%a %H:%M}'.format(start_time)
     return 'In {} @ {}'.format(relative_time, day_time)
 
-# https://stackoverflow.com/a/4830620/486990
+################################################################################
+# Routes:
 
 @app.route('/')
 def index():
-    # Dummy data. Replace with db.
-    # start_time = datetime.datetime.now() + datetime.timedelta(minutes=32)
-    # now = datetime.datetime.now()
-    # delta = relativedelta(start_time, now)
-    # matches = [
-    #     {
-    #         'game' : {
-    #             'title' : 'Exploding Kittens',
-    #             'url' : 'https://boardgamegeek.com/boardgame/172225/exploding-kittens'
-    #         },
-    #         'start_time' : human_readable(start_time),
-    #         'players' : {
-    #             'registered' : ['Thom', 'David'],
-    #             'min' : 2,
-    #             'max' : 5,
-    #         },
-    #         'winner' : None
-    #     }
-    # ]
     matches = get_matches()
     return render_template('index.html', matches=matches)
 
